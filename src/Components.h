b19d3bed7e49a8bd997ae56cbdc231e409e87272
chrisilly <business@culling.se>
2025-04-08 15:38:37 +0200

    clean up component structs

--- src/Components.h
+++ src/Components.h
@@ -12,18 +12,12 @@ struct Transform
     glm::vec3 position;
     glm::vec3 scale;
     glm::mat3 rotation;
-
-public:
-    Transform();
 };
 
 /// @brief Handles linear velocity
 struct Velocity
 {
     glm::vec3 velocity;
-
-public:
-    Velocity();
 };
 
 /// @brief A reference to an existing RendeableMesh, a resource that can be shared/reused between entities
@@ -31,10 +25,6 @@ struct Mesh
 {
     std::shared_ptr<eeng::RenderableMesh> mesh; // if you want to own the resource
     // std::weak_ptr<eeng::RenderableMesh> mesh; // if you want to reference an existing resource without owning it
-    
-public:
-    Mesh();
-    // ~Mesh() { mesh.reset(); } // Destructor to release the weak pointer
 };
 
 #endif
